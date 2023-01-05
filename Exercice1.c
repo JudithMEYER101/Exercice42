59d638bc86f03840a3531dc46d72ad3dba6c3365
Judith MEYER <judith.meyer@outlook.fr>
2023-01-05 15:55:27 +0100

    first commit

---
+++ Exercice1.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 5;
+	b = 2;
+	div = 0;
+	mod = 0;
+	ft_div_mod(a, b, &div, &mod);
+	printf("\n%d\n%d\n", div, mod);
+	return (0);
+}
